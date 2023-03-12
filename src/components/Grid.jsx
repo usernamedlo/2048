e7eb9b9d82bbe0b2c1c0dccace579cb36086db84
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-03-13 00:39:55 +0100

    idk

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
 
-class Grid extends React.Component {
+class Grid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grid: Array(4)
+        .fill()
+        .map(() => Array(4).fill(0)),
+    };
+  }
+
   render() {
+    const { grid } = this.state;
+
     return (
       <div className="grid-container">
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
+        {grid.map((row, rowIndex) => (
+          <div className="grid-row" key={rowIndex}>
+            {row.map((cell, colIndex) => (
+              <div className="grid-cell" key={`${rowIndex}-${colIndex}`}>
+                {cell}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
