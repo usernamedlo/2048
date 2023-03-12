3d78c61d7432b058019497a1343d71877c7bb5b4
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-03-13 00:22:29 +0100

    Initialization repository

---
+++ src/components/Grid.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Grid extends React.Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Grid;
